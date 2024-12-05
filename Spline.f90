fb90f37eb5721218a4c813edb5fe0483f3efb1ad
j-alberola <josepalberolaboloix@gmail.com>
2024-12-05 15:16:28 +0100

    Subroutine to find minima in 1st order energy velocity implemented

--- Spline.f90
+++ Spline.f90
@@ -64,6 +64,9 @@ real*8, intent(out) :: poly_coeff_deriv(3,2)
 integer :: i
 
 !! NO NEED TO PASS THE WHOLE MATRIX
+
+!!!CHECK MATRIX THING
+!!!
 do i = 1,2
 !!REORDE 1 AND 2
    Poly_coeff(1,i) = (2.d0*M(z,i+1)+h*M(z,i+3)-2.d0*M(z+1,i+1)+h*M(z+1,i+3))/h**3
@@ -90,6 +93,7 @@ integer, intent(in) :: z, num_points
 real*8 :: eta_inter
 real*8 :: poly_real, poly_imag, poly_real_deriv, poly_imag_deriv, poly_real_deriv2, poly_imag_deriv2
 integer :: j, np_poly
+real*8 :: coeffs(5),poly
 
 
 !!DREAL or dp as in the complex CHANGE
@@ -102,28 +106,66 @@ do j = 1,np_poly
        poly_real_deriv = interpol_deriv(eta_inter-M(z,1),Poly_coeff_deriv(1,1), Poly_coeff_deriv(2,1), Poly_coeff_deriv(3,1))
        poly_imag_deriv = interpol_deriv(eta_inter-M(z,1),Poly_coeff_deriv(1,2), Poly_coeff_deriv(2,2), Poly_coeff_deriv(3,2))
 
-       poly_real_deriv2 = interpol_deriv2(eta_inter-M(z,1),2.d0*Poly_coeff_deriv(1,1),& 
-                          Poly_coeff_deriv(2,1), eta_inter)
-       poly_imag_deriv2 = interpol_deriv2(eta_inter-M(z,1),2.d0*Poly_coeff_deriv(1,2),& 
-                          Poly_coeff_deriv(2,2), eta_inter)
-        
-       write (*,*) eta_inter, poly_real, poly_imag, poly_real_deriv, poly_imag_deriv,&
-                   poly_real-eta_inter*poly_real_deriv, poly_imag-eta_inter*poly_imag_deriv,&
-                   eta_inter*dsqrt(poly_real_deriv2**2+poly_imag_deriv2**2)
+       call minimum_find_U (eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
+!       poly_real_deriv2 = interpol_deriv2(eta_inter-M(z,1),2.d0*Poly_coeff_deriv(1,1),& 
+        !                  Poly_coeff_deriv(2,1), eta_inter)
+!       poly_imag_deriv2 = interpol_deriv2(eta_inter-M(z,1),2.d0*Poly_coeff_deriv(1,2),& 
+        !                  Poly_coeff_deriv(2,2), eta_inter)
+
+        write (*,*) eta_inter,interpol_deriv_U(eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),M(z,1)),&
+                    interpol_deriv_U(eta_inter,Poly_coeff(1,2),Poly_coeff(2,2),M(z,1)),&
+                    eta_inter*SQRT(interpol_deriv_U(eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),M(z,1))**2+&
+                                   interpol_deriv_U(eta_inter,Poly_coeff(1,2),Poly_coeff(2,2),M(z,1))**2),&
+                    poly 
+    !   write (*,*) eta_inter, poly_real, poly_imag, poly_real_deriv, poly_imag_deriv
+!                   poly_real-eta_inter*poly_real_deriv, poly_imag-eta_inter*poly_imag_deriv,&
+!                   eta_inter*dsqrt(poly_real_deriv2**2+poly_imag_deriv2**2)
                    !eta_inter*dsqrt(poly_real_deriv**2+poly_imag_deriv**2)
 end do
 
 end subroutine
 
 
-real*8 function interpol_deriv2 (t, c1, c2, ss) result (poly)
 
-real*8, intent(in) :: c1, c2,ss
-real*8, intent(in) :: t
+subroutine minimum_find_U (eta,a,b,c,d,eta_1,coeffs,poly)
 
-poly = 2*c1*ss*t**2 + c2*ss**2
+real*8, intent(in) :: eta, a, b, c, d, eta_1
+real*8, intent(out) :: coeffs(5), poly
+
+! Compute the terms step by step
+coeffs (:) = 0.d0
+coeffs(5) = (108.0D0 * a**2 + 108.0D0 * c**2)
+
+coeffs(4) = 12.0D0 * a + 6.0D0 * a * b + 12.0D0 * c + 6.0D0 * c * d - 36.0D0 * a**2 * eta_1 - 36.0D0 * c**2 * eta_1 + &
+    24.0D0 * a * (2.0D0 * b - 6.0D0 * a * eta_1) + 24.0D0 * c * (2.0D0 * d - 6.0D0 * c * eta_1)
+
+coeffs(3) = 2.0D0 * (2.0D0 * b - 6.0D0 * a * eta_1) + b * (2.0D0 * b - 6.0D0 * a * eta_1) - 6.0D0 * a * eta_1 *&
+        (2.0D0 * b - 6.0D0*a * eta_1) + &
+    (2.0D0 * b - 6.0D0 * a * eta_1)**2 + 2.0D0 * (2.0D0 * d - 6.0D0 * c * eta_1) + d * (2.0D0 * d - 6.0D0 * c * eta_1) - &
+    6.0D0 * c * eta_1 * (2.0D0 * d - 6.0D0 * c * eta_1) + (2.0D0 * d - 6.0D0 * c * eta_1)**2
+
+! Final result
+poly = coeffs(5)*eta**4 + coeffs(4)*eta**3 + coeffs(3)*eta**2
+
+!print *, "The result is:", result
+
+
+end subroutine
+
+
+
+
+
+real*8 function interpol_deriv_U (eta, c1, c2, eta1) result (poly)
+
+real*8, intent(in) :: c1, c2
+real*8, intent(in) :: eta, eta1
+
+poly = 6.d0*c1*eta**2+(2*c2-6.d0*eta1*c1)*eta
 
 end function
+
+
 subroutine polynomial_evaluation_minima (point_min,initial_point,Poly_coeff,Poly_coeff_deriv)
 
 real*8, intent(in) :: point_min, initial_point
@@ -181,13 +223,21 @@ real*8 :: coeffs(5)
 real*8 :: Poly_coeff(4,2),Poly_coeff_deriv(3,2)
 real*8 :: eta_step
 real*8 :: minimum, initial_point
-integer :: z, i 
+integer :: z, i,j
+real*8 :: eta, poly
 
 do z = 1, num_points-1
        eta_step = M(z+1,1)-M(z,1)
-       call polynomials_coefficients (M,num_points,eta_step,z,Poly_coeff,poly_coeff_deriv) 
-       call velocity_deriv (M(z,1),Poly_coeff_deriv(1,1), Poly_coeff_deriv(2,1), Poly_coeff_deriv(3,1),&
-                           Poly_coeff_deriv(1,2), Poly_coeff_deriv(2,2), Poly_coeff_deriv(3,2),coeffs)
+       call polynomials_coefficients (M,num_points,eta_step,z,Poly_coeff,poly_coeff_deriv)
+!do j = 0,40
+ !      eta = M(z,1)+eta_step*0.025*j
+ !      call velocity_deriv (eta,M(z,1),Poly_coeff_deriv(1,1), Poly_coeff_deriv(2,1), Poly_coeff_deriv(3,1),&
+!                           Poly_coeff_deriv(1,2), Poly_coeff_deriv(2,2), Poly_coeff_deriv(3,2),coeffs,poly)
+  !     write (*,*) 'Poly', eta, poly
+!end do
+
+       call minimum_find_U (1.d0,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
+
        call QuarticRoots(Coeffs,zeros)
        do i =1,4
           if (AIMAG(zeros(i)) .eq. 0.d0 .and. DBLE(zeros(i)) .gt. M(z,1) .and. DBLE(zeros(i)) .lt. M(z+1,1)) then
@@ -228,14 +278,14 @@ end function
 !
 ! a,b,c and d,e,f are the coefficents of the derivative of the real and the complex polynomial
 !
-subroutine velocity_deriv (eta1,a,b,c,d,e,f,coeffs)
+subroutine velocity_deriv (eta,eta1,a,b,c,d,e,f,coeffs,poly)
 
-!real*8, intent(in) :: eta
+real*8, intent(in) :: eta
 real*8, intent(in) :: eta1
 real*8, intent(in) :: a,b,c
 real*8, intent(in) :: d,e,f
 real*8, intent(out) :: coeffs(5)
-!real*8, intent(out) :: poly
+real*8, intent(out) :: poly
 
 coeffs(5) = 3.d0*a**2+3.d0*d**2
 coeffs(4) = 5.d0*a*b+5.d0*d*e-10.d0*a**2*eta1-10.d0*d**2*eta1
@@ -249,7 +299,7 @@ coeffs(1) = c**2+f**2-2.d0*b*c*eta1-2.d0*e*f*eta1+b**2*eta1**2+&
          2.d0*d*e*eta1**3+a**2*eta1**4+d**2*eta1**4
 
 
-!poly = coeffs(5)*eta**4.d0+coeffs(4)*eta**3.d0+coeffs(3)*eta**2.d0+coeffs(2)*eta+coeffs(1)
+poly = coeffs(5)*eta**4.d0+coeffs(4)*eta**3.d0+coeffs(3)*eta**2.d0+coeffs(2)*eta+coeffs(1)
 
 end subroutine
 

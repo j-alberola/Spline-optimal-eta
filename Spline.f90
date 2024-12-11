76b6a5786981098caa882004c6dde2d84658e2f3
j-alberola <josepalberolaboloix@gmail.com>
2024-12-11 11:42:48 +0100

    Ordered the code a little bit for better understanding + worked on the mminima find subroutine

--- Spline.f90
+++ Spline.f90
@@ -1,8 +1,8 @@
 module spline_module
 
-use PolynomialRoots 
+use PolynomialRoots
 
-implicit none 
+implicit none
 
 
 integer, parameter ::dp = kind(0.d0)
@@ -15,16 +15,16 @@ contains
 !
 subroutine Reading (M,num_points)
 
-real*8, allocatable, intent(out) :: M (:,:) 
+real*8, allocatable, intent(out) :: M (:,:)
 integer, intent(out) :: num_points
 integer :: i, ios
-character(len=30) :: file_name
+character(len=1000) :: file_name
 character(len=1) :: foo
 
 
 write (6,*) 'Insert file name'
 read(5,*) file_name
-        
+
 open (UNIT=10, FILE=file_name)
 
 num_points = 0
@@ -41,7 +41,7 @@ allocate (M(num_points, 5))
 do i = 1, num_points
         read (10, *) M (i, :)
 !
-!  In the quantum package output, the derivatives are stored as -eta*(dE/deta) 
+!  In the quantum package output, the derivatives are stored as -eta*(dE/deta)
 !
         M (i,4) = -M(i,4)/M(i,1)
         M (i,5) = -M(i,5)/M(i,1)
@@ -99,63 +99,25 @@ real*8 :: coeffs(5),poly
 !!DREAL or dp as in the complex CHANGE
 do j = 1,np_poly
        eta_inter = M(z,1)*(REAL(np_poly-1,8)-REAL(j-1,8))/(REAL(np_poly-1,8)) + M(z+1,1)*(REAL(j-1,8)/(REAL(np_poly-1,8)))
-       
+
        poly_real = interpol(eta_inter-M(z,1),Poly_coeff(1,1), Poly_coeff(2,1), Poly_coeff(3,1), Poly_coeff(4,1))
-       poly_imag = interpol(eta_inter-M(z,1),Poly_coeff(1,2), Poly_coeff(2,2), Poly_coeff(3,2), Poly_coeff(4,2)) 
-         
+       poly_imag = interpol(eta_inter-M(z,1),Poly_coeff(1,2), Poly_coeff(2,2), Poly_coeff(3,2), Poly_coeff(4,2))
+
        poly_real_deriv = interpol_deriv(eta_inter-M(z,1),Poly_coeff_deriv(1,1), Poly_coeff_deriv(2,1), Poly_coeff_deriv(3,1))
        poly_imag_deriv = interpol_deriv(eta_inter-M(z,1),Poly_coeff_deriv(1,2), Poly_coeff_deriv(2,2), Poly_coeff_deriv(3,2))
 
-       call minimum_find_U (eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
+       call velocity_deriv_U (eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
 
        write (*,*) eta_inter,interpol_deriv_U(eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),M(z,1)),&
                     interpol_deriv_U(eta_inter,Poly_coeff(1,2),Poly_coeff(2,2),M(z,1)),&
                     eta_inter*SQRT(interpol_deriv_U(eta_inter,Poly_coeff(1,1),Poly_coeff(2,1),M(z,1))**2+&
                                    interpol_deriv_U(eta_inter,Poly_coeff(1,2),Poly_coeff(2,2),M(z,1))**2),&
-                    poly 
+                    poly
 end do
 
 end subroutine
 
 
-
-subroutine minimum_find_U (eta,a,b,c,d,eta_1,coeffs,poly)
-
-real*8, intent(in) :: eta, a, b, c, d, eta_1
-real*8, intent(out) :: coeffs(5), poly
-
-! Compute the terms step by step
-coeffs (:) = 0.d0
-coeffs(5) = (108.0D0 * a**2 + 108.0D0 * c**2)
-
-coeffs(4) = 12.0D0 * a + 6.0D0 * a * b + 12.0D0 * c + 6.0D0 * c * d - 36.0D0 * a**2 * eta_1 - 36.0D0 * c**2 * eta_1 + &
-    24.0D0 * a * (2.0D0 * b - 6.0D0 * a * eta_1) + 24.0D0 * c * (2.0D0 * d - 6.0D0 * c * eta_1)
-
-coeffs(3) = 2.0D0 * (2.0D0 * b - 6.0D0 * a * eta_1) + b * (2.0D0 * b - 6.0D0 * a * eta_1) - 6.0D0 * a * eta_1 *&
-        (2.0D0 * b - 6.0D0*a * eta_1) + &
-    (2.0D0 * b - 6.0D0 * a * eta_1)**2 + 2.0D0 * (2.0D0 * d - 6.0D0 * c * eta_1) + d * (2.0D0 * d - 6.0D0 * c * eta_1) - &
-    6.0D0 * c * eta_1 * (2.0D0 * d - 6.0D0 * c * eta_1) + (2.0D0 * d - 6.0D0 * c * eta_1)**2
-
-! Final result
-poly = coeffs(5)*eta**4 + coeffs(4)*eta**3 + coeffs(3)*eta**2
-
-!print *, "The result is:", result
-
-
-end subroutine
-
-
-
-
-
-real*8 function interpol_deriv_U (eta, c1, c2, eta1) result (poly)
-
-real*8, intent(in) :: c1, c2
-real*8, intent(in) :: eta, eta1
-
-poly = 6.d0*c1*eta**2+(2*c2-6.d0*eta1*c1)*eta
-
-end function
 
 
 subroutine polynomial_evaluation_minima (point_min,initial_point,Poly_coeff,Poly_coeff_deriv)
@@ -217,29 +179,34 @@ real*8 :: minimum, initial_point
 integer :: z, i,j
 real*8 :: eta, poly
 
+!
+! VARIABLE USED IN CASE EVALUATING THE DEIVATIVE OF THE VELOCITY AT GIVEN ETA VALUES IS NEEDED
+eta = 1.d0
+
 do z = 1, num_points-1
        eta_step = M(z+1,1)-M(z,1)
        call polynomials_coefficients (M,num_points,eta_step,z,Poly_coeff,poly_coeff_deriv)
        call velocity_deriv (eta,M(z,1),Poly_coeff_deriv(1,1), Poly_coeff_deriv(2,1), Poly_coeff_deriv(3,1),&
                           Poly_coeff_deriv(1,2), Poly_coeff_deriv(2,2), Poly_coeff_deriv(3,2),coeffs,poly)
 
-       call minimum_find_U (1.d0,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
+!       call velocity_deriv_U (1.d0,Poly_coeff(1,1),Poly_coeff(2,1),Poly_coeff(1,2),Poly_coeff(2,2),M(z,1),coeffs,poly)
 
        call QuarticRoots(Coeffs,zeros)
+!       write (*,*) zeros
        do i =1,4
           if (AIMAG(zeros(i)) .eq. 0.d0 .and. DBLE(zeros(i)) .gt. M(z,1) .and. DBLE(zeros(i)) .lt. M(z+1,1)) then
-             WRITe (*,*) " MINIMA FOUND"
+!             WRITe (*,*) " MINIMA FOUND"
              minimum = DBLE(zeros(i))
-             initial_point = M(z,1)
-             call polynomial_evaluation_minima (minimum,initial_point,Poly_coeff,poly_coeff_deriv)
+             if (velocity_deriv2(coeffs,DBLE(zeros(i))) .gt. 0.d0) then
+                initial_point = M(z,1)
+                call polynomial_evaluation_minima (minimum,initial_point,Poly_coeff,poly_coeff_deriv)
+             end if
           end if
        end do
 
 end do
 
 end subroutine
-
-
 
 !!PUT INTENTS
 real*8 function interpol (t, c1, c2, c3 ,c4) result (poly)
@@ -258,6 +225,15 @@ real*8, intent(in) :: c1, c2, c3
 real*8, intent(in) :: t
 
 poly = c1*t**2 + c2*t + c3
+
+end function
+
+real*8 function interpol_deriv_U (eta, c1, c2, eta1) result (poly)
+
+real*8, intent(in) :: c1, c2
+real*8, intent(in) :: eta, eta1
+
+poly = 6.d0*c1*eta**2+(2*c2-6.d0*eta1*c1)*eta
 
 end function
 
@@ -286,9 +262,47 @@ coeffs(1) = c**2+f**2-2.d0*b*c*eta1-2.d0*e*f*eta1+b**2*eta1**2+&
          2.d0*d*e*eta1**3+a**2*eta1**4+d**2*eta1**4
 
 
-poly = coeffs(5)*eta**4.d0+coeffs(4)*eta**3.d0+coeffs(3)*eta**2.d0+coeffs(2)*eta+coeffs(1)
+poly = coeffs(5)*eta**4+coeffs(4)*eta**3+coeffs(3)*eta**2+coeffs(2)*eta+coeffs(1)
 
 end subroutine
+
+real*8 function velocity_deriv2 (coeffs, eta) result (res)
+
+real*8, intent(in) :: coeffs(5), eta
+
+
+res = 4.d0*coeffs(5)*eta**3+3.d0*coeffs(4)*eta**2+2.d0*coeffs(3)*eta+coeffs(2)
+
+end function
+
+
+subroutine velocity_deriv_U (eta,a,b,c,d,eta_1,coeffs,poly)
+
+real*8, intent(in) :: eta, a, b, c, d, eta_1
+real*8, intent(out) :: coeffs(5), poly
+
+! Compute the terms step by step
+coeffs (:) = 0.d0
+coeffs(5) = (108.0D0 * a**2 + 108.0D0 * c**2)
+
+coeffs(4) = 12.0D0 * a + 6.0D0 * a * b + 12.0D0 * c + 6.0D0 * c * d - 36.0D0 * a**2 * eta_1 - 36.0D0 * c**2 * eta_1 + &
+    24.0D0 * a * (2.0D0 * b - 6.0D0 * a * eta_1) + 24.0D0 * c * (2.0D0 * d - 6.0D0 * c * eta_1)
+
+coeffs(3) = 2.0D0 * (2.0D0 * b - 6.0D0 * a * eta_1) + b * (2.0D0 * b - 6.0D0 * a * eta_1) - 6.0D0 * a * eta_1 *&
+        (2.0D0 * b - 6.0D0*a * eta_1) + &
+    (2.0D0 * b - 6.0D0 * a * eta_1)**2 + 2.0D0 * (2.0D0 * d - 6.0D0 * c * eta_1) + d * (2.0D0 * d - 6.0D0 * c * eta_1) - &
+    6.0D0 * c * eta_1 * (2.0D0 * d - 6.0D0 * c * eta_1) + (2.0D0 * d - 6.0D0 * c * eta_1)**2
+
+! Final result
+poly = coeffs(5)*eta**4 + coeffs(4)*eta**3 + coeffs(3)*eta**2
+
+!print *, "The result is:", result
+
+
+end subroutine
+
+
+
 
 subroutine evaluation_at_eta_value(M,num_points)
 
@@ -316,7 +330,7 @@ read(*,*) evaluated_eta
 !  ADD A WARNING IN CASE THE VALUE OF EVALUATED_ETA IS OUTSIDE THE INTERVAL OF GIVEN POINTS
 !
 do z = 1, num_points-1
- 
+
 if (evaluated_eta .ge. M(z,1) .and. evaluated_eta .le. M(z+1,1)) then
 
 eta_step = M(z+1,1)-M(z,1)
@@ -325,7 +339,7 @@ segment_saved = z
 end if
 
 
-end do 
+end do
 
 call polynomials_coefficients (M,num_points,eta_step,segment_saved,Poly_coeff,poly_coeff_deriv)
 call polynomial_evaluation_minima (evaluated_eta,M(segment_saved,1),Poly_coeff,Poly_coeff_deriv)
@@ -340,14 +354,14 @@ end module
 
 program Spline
 
-Use spline_module  
+Use spline_module
 
 implicit none
 
 
 real*8, allocatable :: M(:,:)
 integer :: num_points
-integer :: i 
+integer :: i
 character(len=50) :: option, option2
 
 
@@ -357,6 +371,7 @@ character(len=50) :: option, option2
 !
 
 call Reading (M,num_points)
+!write(*,*) 'Debug'
 
 !do i = 1, num_points
 !       write (*,*) M(i,:)
@@ -367,10 +382,10 @@ read(5,*) option
 if (option .eq. 'Polynomial_fitting') then
     call fitting (M,num_points)
 else if (option .eq. 'Minima_find') then
-    call minimum_find (M,num_points) 
+    call minimum_find (M,num_points)
 else if (option .eq. 'Neutral_Interpolation') then
     call evaluation_at_eta_value(M,num_points)
-else 
+else
     write (*,*) 'Bad option chosen, try again'
 end if
 
@@ -382,5 +397,4 @@ end if
 
 
 end program
-
 

ec85f80a5db21b838a6ce35c33fc026aee8f423a
j-alberola <josepalberolaboloix@gmail.com>
2024-12-18 11:38:33 +0100

    Now properly reads the exact value of eta used in each calculation

--- script_scan.sh
+++ script_scan.sh
@@ -32,31 +32,37 @@ qp set determinants read_wf True
 #qp set determinants n_states 3
 
 qp run diagonalize_h_cap > $1.scan.out
+cp $1.scan.out $1.scan.script.1st.out
 ./script_data_corrected.sh $1 2 1
-./a.out << EOF > Minimum.dat
+./Spline.x << EOF
 $1.data1.good
 Minima_find
 EOF
 threshold=0.00003
 qp set cap eta_step_size 0.
 qp set cap n_steps_cap  1
-old_energy=$(awk 'NR == 3 {print $2}' Minimum.dat)
+old_energy=$(awk 'NR == 1 {print $2}' Minima.dat)
 
 
 
 while true; do
 
-new_min=$(awk 'NR == 3 {print $1}' Minimum.dat)
+
+new_min=$(awk 'NR == 1 || $6 < min {min = $6; val = $1} END {print val}' Minima.dat)
+echo $new_min > min_file
+rm -f Minima.dat
 qp set cap eta_cap $new_min
 qp run diagonalize_h_cap > $1.scan.out
+cp $1.scan.out $1.scan.script.$new_min.out
 ./script_data_corrected.sh $1 1 2
-cat $1.data2.good >> $1.data1.good
+awk {'print $2, $3, $4, $5, $6'} $1.data2.good > energy_file
+paste min_file energy_file >> $1.data1.good
 sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
-./a.out << EOF > Minimum.dat
+./Spline.x << EOF
 $1.data1.good.sorted
 Minima_find
 EOF
-new_energy=$(awk 'NR == 3 {print $2}' Minimum.dat)
+new_energy=$(awk 'NR == 1 {print $2}' $1.data2.good)
 
 # Calculate energy difference
 energy_diff=$(awk "BEGIN {print $new_energy - $old_energy}")
@@ -65,14 +71,19 @@ if [ $(awk "BEGIN {if ($energy_diff < 0) print 1; else print 0}") -eq 1 ]; then
     energy_diff=$(awk "BEGIN {print -1*$energy_diff}")
 fi
 echo $energy_diff
+echo $new_min
 # Compare with a threshold
+# REVISE THIS PART
 if [ $(awk "BEGIN {if ($energy_diff < $threshold) print 1; else print 0}") -eq 1 ]; then
     echo "Convergence reached. Energy difference: $energy_diff"
-    new_min=$(awk 'NR == 3 {print $1}' Minimum.dat)
+    new_min=$(awk 'NR == 1 || $6 < min {min = $6; val = $1} END {print val}' Minima.dat)
+    echo $new_min > min_file
     qp set cap eta_cap $new_min
     qp run diagonalize_h_cap > $1.scan.out
+    cp $1.scan.out $1.scan.script.$new_min.out
     ./script_data_corrected.sh $1 1 2
-    cat $1.data2.good >> $1.data1.good
+    awk {'print $2, $3, $4, $5, $6, "FINAL_MINIMA"'} $1.data2.good > energy_file
+    paste min_file energy_file >> $1.data1.good
     sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
     break
 fi
@@ -80,7 +91,10 @@ fi
 old_energy=$new_energy
 done
 
-
+rm -f $1.data2.good
+rm -f energy_file
+rm -f min_file
+rm -f Minima.dat
 
 
 

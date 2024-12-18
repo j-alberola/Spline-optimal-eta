f5e181f67407812ec8f2461f3e6832fb82f9bb45
j-alberola <josepalberolaboloix@gmail.com>
2024-12-18 11:39:35 +0100

    Now removes unused unncesary in the end

--- script_data_corrected.sh
+++ script_data_corrected.sh
@@ -9,8 +9,10 @@
 grep -A$(( $2+10 )) 'CAP energies' $1.scan.out | tail -$2 | awk {'print $1, $4, $5'} > $1.data
 
 
-grep -A$(( $2+7 )) 'Energy corrections' $1.scan.out | tail -$2 | awk {'print $4, $5'} > $1.corrections.data
+grep -A$(( $2+7 )) 'Energy corrections' $1.scan.out | tail -$2 | awk {'print $4, $5, $1*sqrt(($4/$1)^2+($5/$1)^2)'} > $1.corrections.data
 
 
 paste $1.data $1.corrections.data >$1.data$3.good
 
+rm -f $1.data
+rm -f $1.corrections.data

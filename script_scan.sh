0a117426c82ad1cd1ec1d2043920b7ff758b4043
j-alberola <josepalberolaboloix@gmail.com>
2025-01-16 10:45:04 +0100

    Rewrote to be able to find multiple minima

--- script_scan.sh
+++ script_scan.sh
@@ -37,64 +37,114 @@ cp $1.scan.out $1.scan.script.1st.out
 ./Spline.x << EOF
 $1.data1.good
 Minima_find
+0
 EOF
+
 threshold=0.00003
 qp set cap eta_step_size 0.
 qp set cap n_steps_cap  1
-old_energy=$(awk 'NR == 1 {print $2}' Minima.dat)
 
 
 
-while true; do
+#  ADD A LOOP HERE
+#  MAYBE REMOVE THE DIFFERENT 2 CASES, JUST 1 IS ENOUGH
+
+iter = 1
+while iter < 20 ; do
 
 
-new_min=$(awk 'NR == 1 || $6 < min {min = $6; val = $1} END {print val}' Minima.dat)
-echo $new_min > min_file
-rm -f Minima.dat
-qp set cap eta_cap $new_min
-qp run diagonalize_h_cap > $1.scan.out
-cp $1.scan.out $1.scan.script.$new_min.out
-./script_data_corrected.sh $1 1 2
-awk {'print $2, $3, $4, $5, $6'} $1.data2.good > energy_file
-paste min_file energy_file >> $1.data1.good
-sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
-./Spline.x << EOF
-$1.data1.good.sorted
-Minima_find
-EOF
-new_energy=$(awk 'NR == 1 {print $2}' $1.data2.good)
+if [ -f "Minima_converged.dat" ]; then
+awk -v thresh=0.01 '
+BEGIN {
+    while ((getline < "Minima_converged.dat") > 0) {
+        file2_values[$1] = 1;  # Store all values from file2
+    }
+}
+{
+    for (val in file2_values) {
+        diff = ($1 > val) ? $1 - val : val - $1;  # Absolute difference
+        if (diff <= $1 * thresh) {
+            next;  # Skip the line if within 1% interval
+        }
+    }
+    print $0;  # Print the line if no match within 1%
+}' "Minima.dat" > aux_file
 
-# Calculate energy difference
-energy_diff=$(awk "BEGIN {print $new_energy - $old_energy}")
-echo $energy_diff
-if [ $(awk "BEGIN {if ($energy_diff < 0) print 1; else print 0}") -eq 1 ]; then
-    energy_diff=$(awk "BEGIN {print -1*$energy_diff}")
+cat aux_file > Minima.dat
+rm -f aux_file
+
 fi
-echo $energy_diff
-echo $new_min
-# Compare with a threshold
-# REVISE THIS PART
-if [ $(awk "BEGIN {if ($energy_diff < $threshold) print 1; else print 0}") -eq 1 ]; then
-    echo "Convergence reached. Energy difference: $energy_diff"
-    new_min=$(awk 'NR == 1 || $6 < min {min = $6; val = $1} END {print val}' Minima.dat)
-    echo $new_min > min_file
-    qp set cap eta_cap $new_min
-    qp run diagonalize_h_cap > $1.scan.out
-    cp $1.scan.out $1.scan.script.$new_min.out
-    ./script_data_corrected.sh $1 1 2
-    awk {'print $2, $3, $4, $5, $6, "FINAL_MINIMA"'} $1.data2.good > energy_file
-    paste min_file energy_file >> $1.data1.good
-    sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
+#
+#  If there are no remaining minima, exit from the loop
+#
+if [ -z "$(head -n 1 Minima.dat)" ]; then
     break
 fi
 
-old_energy=$new_energy
+fit_real_energy=$(awk 'NR == 1 {print $2}' Minima.dat)
+fit_imag_energy=$(awk 'NR == 1 {print $3}' Minima.dat)
+fit_real_deriv=$(awk 'NR == 1 {print $4}' Minima.dat)
+fit_imag_deriv=$(awk 'NR == 1 {print $5}' Minima.dat)
+new_min=$(awk 'NR == 1 {print $1}' Minima.dat)
+qp set cap eta_cap $new_min
+echo $new_min > min_file
+qp run diagonalize_h_cap > $1.scan.out
+#REMOVE WHEN NOT DEBUGGINMG
+cp $1.scan.out $1.scan.script.$new_min.out
+./script_data_corrected.sh $1 1 2
+qp_real_energy=$(awk 'NR == 1 {print $2}' $1.data2.good)
+qp_imag_energy=$(awk 'NR == 1 {print $3}' $1.data2.good)
+qp_real_deriv=$(awk 'NR == 1 {print $4}' $1.data2.good)
+qp_imag_deriv=$(awk 'NR == 1 {print $5}' $1.data2.good)
+#Calculating differences between fit and qp energies and derivatives in absolute value
+real_energy_diff=$(awk "BEGIN {print $qp_real_energy - $fit_real_energy}")
+if [ $(awk "BEGIN {if ($real_energy_diff < 0) print 1; else print 0}") -eq 1 ]; then
+    real_energy_diff=$(awk "BEGIN {print -1*$real_energy_diff}")
+fi
+imag_energy_diff=$(awk "BEGIN {print $qp_imag_energy - $fit_imag_energy}")
+if [ $(awk "BEGIN {if ($imag_energy_diff < 0) print 1; else print 0}") -eq 1 ]; then
+    imag_energy_diff=$(awk "BEGIN {print -1*$imag_energy_diff}")
+fi
+real_deriv_diff=$(awk "BEGIN {print $qp_real_deriv - $fit_real_deriv}")
+if [ $(awk "BEGIN {if ($real_deriv_diff < 0) print 1; else print 0}") -eq 1 ]; then
+    real_deriv_diff=$(awk "BEGIN {print -1*$real_deriv_diff}")
+fi
+imag_deriv_diff=$(awk "BEGIN {print $qp_imag_deriv - $fit_imag_deriv}")
+if [ $(awk "BEGIN {if ($imag_deriv_diff < 0) print 1; else print 0}") -eq 1 ]; then
+    imag_deriv_diff=$(awk "BEGIN {print -1*$imag_deriv_diff}")
+fi
+
+
+# If all the conditions are fulfilled, exit from the loop
+if [ $(awk "BEGIN {if ($real_energy_diff < $threshold && $imag_energy_diff < $threshold && $real_deriv_diff < $threshold && $imag_deriv_diff < $threshold) print 1; else print 0}") -eq 1 ]; then
+        echo "Convergence reached"
+        echo $new_min
+        awk {'print $2, $3, $4, $5, $6, FINAL MINIMA'} $1.data2.good > energy_file
+        paste min_file energy_file >> $1.data1.good
+        paste min_file energy_file >> Minima_converged.dat
+        sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
+	./Spline.x << EOF
+        $1.data1.good
+        Minima_find
+        0
+EOF
+
+else
+        awk {'print $2, $3, $4, $5, $6'} $1.data2.good > energy_file
+        paste min_file energy_file >> $1.data1.good
+        sort -n -k1,1 $1.data1.good > $1.data1.good.sorted
+        ./Spline.x << EOF
+        $1.data1.good
+        Minima_find
+        0
+EOF
+fi
+
+iter = $iter + 1
 done
 
 rm -f $1.data2.good
 rm -f energy_file
 rm -f min_file
 rm -f Minima.dat
-
-
 
